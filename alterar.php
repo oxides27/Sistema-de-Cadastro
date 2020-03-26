1d9685bf1a66f51e9a5214e912ce968e9c41db57
Bruno Rodrigues <brunorodriguesra@outlook.com>
2020-03-26 14:26:20 -0300

    Add files via upload

---
+++ alterar.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Alterar Cadastro </title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+
+<body>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Alterar Cadastro</h3>
+                    <div class="box">
+                        <form action="cadastrar.php" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <input name="nome" type="text" class="input is-large" placeholder="Nome" autofocus>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input name="sobrenome" type="text" class="input is-large" placeholder="Sobrenome">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input name="email" type="text" class="input is-large" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" type="password" placeholder="Senha">
+                                </div>
+                            </div>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Alterar</button>
+                            <br><br>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Excluir</button>
+                        </form>
+                        <form action="alterar.php" method="POST">
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
