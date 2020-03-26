106d9f554fd1de033e4f5b9b7a6e134f5444e82b
Bruno Rodrigues <brunorodriguesra@outlook.com>
2020-03-26 15:31:54 -0300

    Add files via upload

---
+++ dump.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE login;
+USE login;
+
+CREATE TABLE `login`.`nome` (
+  `nome` VARCHAR(200) NOT NULL,
+  `sobrenome` VARCHAR(32) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `senha` VARCHAR(100) NOT NULL,
+  `data_cadastro` DATETIME NOT NULL,
+  PRIMARY KEY (`nome`));
+
+INSERT INTO `usuario` (`nome`,`sobrenome`,`email`, `senha`, `data_cadastro`) VALUES ();
