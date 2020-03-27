7bae7d3e90dae447e5d7612f0668b23fd6daf4e6
Bruno Rodrigues <brunorodriguesra@outlook.com>
2020-03-27 15:04:38 -0300

    Update excluir.php

--- public/excluir.php
+++ public/excluir.php
@@ -5,7 +5,7 @@ function delete($conexao, $nome){
 }
 
 $id = isset($_GET['nome']) && ctype_digit($_GET['nome']) ? $_GET['nome'] : null;
-if(delete($conexao, $id)){
+if(delete($conexao, $nome)){
    header('alterar.php');
 }else{
    echo 'erro ao excluír o registro';
